b4076e1f4c72c9b903e331378831ec5647510a2f
Konstantin Stolyarenko <stolyarenko.kons@gmail.com>
2025-06-09 13:07:03 +0200

    Added static mock extension for Dish model

--- app/src/main/java/com/sks/littlelemon/models/Dish.kt
+++ app/src/main/java/com/sks/littlelemon/models/Dish.kt
@@ -1,9 +1,23 @@
 package com.sks.littlelemon.models
 
+import com.sks.littlelemon.R
+
 data class Dish(
     val id: Int,
     val nameResourceId: Int,
     val descriptionResourceId: Int,
     val price: Double,
     val imageResource: Int
-)
+) {
+    companion object
+}
+
+fun Dish.Companion.mock(): Dish {
+    return Dish(
+        id = 1,
+        nameResourceId = R.string.greek_salad,
+        descriptionResourceId = R.string.greek_salad_description,
+        price = 12.99,
+        imageResource = R.drawable.greeksalad
+    )
+}
